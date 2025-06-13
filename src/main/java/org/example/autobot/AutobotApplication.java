3188979e8e647ab0cdc56eb0285fb433b9cdbc59
abdullo12 <90553949+abdullo12@users.noreply.github.com>
2025-06-13 13:44:01 +0300

    Add Spring Boot setup and Telegram bot integration

---
+++ src/main/java/org/example/autobot/AutobotApplication.java
@@ -0,0 +1,11 @@
+package org.example.autobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutobotApplication.class, args);
+    }
+}
